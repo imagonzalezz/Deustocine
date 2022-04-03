12c9ce4c90f68ce14eef7c3b2891d12fa2e29bec
Imanol Gonzalez <imanolgvallejo@gmail.com>
2022-04-03 19:52:56 +0200

    implementacion de BD y log4j

--- deustocine/src/main/java/com/deustocine/app/Main.java
+++ deustocine/src/main/java/com/deustocine/app/Main.java
@@ -1,7 +1,11 @@
 package com.deustocine.app;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
+	
+	private static Logger LogJava= Logger.getLogger(Main.class);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("");
