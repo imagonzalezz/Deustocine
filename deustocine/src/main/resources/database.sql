770b4c65582e38a75eec0883bb69dc062ddd8a99
Iker Yañez <ikeryanez@opendeusto.es>
2022-04-11 12:43:49 +0200

    Archivo sql actualizado

--- deustocine/src/main/resources/database.sql
+++ deustocine/src/main/resources/database.sql
@@ -1,3 +1,7 @@
-CREATE DATABASE deustocine;
-CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'deusto';
+DROP SCHEMA IF EXISTS deustocine;
+DROP USER 'root'@'localhost';
+
+CREATE SCHEMA deustocine;
+
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'deusto';
 GRANT ALL ON deustocine.* TO 'root'@'localhost';
