e8b72c192dd002ee14e972f1a5274141f36aebe6
Lander <aguir@10.164.62.59>
2022-05-04 12:42:24 +0200

    Test clase cine

---
+++ src/test/java/com/deustocine/app/domain/CineTest.java
@@ -0,0 +1,56 @@
+package com.deustocine.app.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.deustocine.app.domain.Cine;
+
+public class CineTest {
+
+	private Cine c, c2;
+	
+	@Before
+	public void setUp() {
+		c = new Cine();
+		c2 = new Cine();
+	}
+	
+	@Test
+	public void testCod() {
+		c.setCod(1);
+		assertEquals(c.getCod(),1);
+		
+		c2.setCod(2);
+		assertEquals(c2.getCod(),2);
+	}
+	
+	
+	@Test
+	public void testNombre() {
+		c.setNombre("ABC");
+		assertTrue(c.getNombre().equals("ABC"));
+		c2.setNombre("CBA");
+		assertTrue(c2.getNombre().equals("CBA"));
+	}
+	
+	@Test
+	public void testDireccion() {
+		c.setDireccion("AAA 12");
+		assertTrue(c.getDireccion().equals("AAA 12"));
+		c2.setDireccion("BBB 12");
+		assertTrue(c2.getDireccion().equals("BBB 12"));
+	}
+	@Test
+	public void testTelefono() {
+		c.setTelefono(666666666);
+		assertEquals(c.getTelefono(),666666666);
+		
+		c2.setTelefono(999999999);
+		assertEquals(c2.getTelefono(),999999999);
+	}
+	
+	
+}
