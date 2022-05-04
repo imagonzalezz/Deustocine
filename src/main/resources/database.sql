d1dfcfdfd8b92e568579f316f39ebcaf0bbb00b6
Imanol Gonzalez <imanolgvallejo@gmail.com>
2022-05-04 12:36:45 +0200

    properties pom.xml + actualizacion DB

--- src/main/resources/database.sql
+++
@@ -1,7 +0,0 @@
-DROP SCHEMA IF EXISTS deustocine;
-DROP USER 'root'@'localhost';
-
-CREATE SCHEMA deustocine;
-
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'deusto';
-GRANT ALL ON deustocine.* TO 'root'@'localhost';
