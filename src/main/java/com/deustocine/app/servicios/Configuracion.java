5560f974aeec74f471de59b304ca49e88da887d7
Iker Yañez <ikeryanez@opendeusto.es>
2022-05-04 12:14:26 +0200

    Configuracion para servicios

---
+++ src/main/java/com/deustocine/app/servicios/Configuracion.java
@@ -0,0 +1,9 @@
+package com.deustocine.app.servicios;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("deustocine")
+public class Configuracion extends Application {
+
+}
