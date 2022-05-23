5386535c7b9f479419bf76d7162841f213eab550
inigo-aldayturriaga <ialdaytu@opendeusto.es>
2022-05-23 20:42:57 +0200

    Cambios Servidor y Servicios y actualizacion Main Cliente

--- src/main/java/com/deustocine/app/cliente/main/MainCliente.java
+++ src/main/java/com/deustocine/app/cliente/main/MainCliente.java
@@ -1,5 +1,18 @@
 package com.deustocine.app.cliente.main;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import com.deustocine.app.cliente.ventanas.InicioSesion;
+
 public class MainCliente {
+	
+	public static void main(String[] args) {
+		Client c= ClientBuilder.newClient();
+		WebTarget wt =c.target(String.format("http://%s:%s/rest", "localhost","8080"));
+		System.out.println(wt.toString());
+		InicioSesion is = new InicioSesion(c, wt);
+	}
 
 }
