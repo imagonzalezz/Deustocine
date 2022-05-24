50028a046823a26580e837192b2180627d51e5c0
Lander <aguir@192.168.1.128>
2022-05-24 16:40:24 +0200

    Peliculas controller documentacion

--- src/main/java/com/deustocine/app/cliente/controller/PeliculasController.java
+++ src/main/java/com/deustocine/app/cliente/controller/PeliculasController.java
@@ -14,18 +14,23 @@ import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
 import com.deustocine.app.domain.Pelicula;
-
+/**
+ * @author Lander
+ * Clase que implementa la logica de la ventana peliculas
+ * Permite mostrar las peliculas existentes
+ * Por cada sesion que exista se creara un panel
+ */
 public class PeliculasController {
 
 	private WebTarget webTarget;
-	private List<Pelicula> peliculas;
+	private List<Pelicula> peliculas;//lista de peliculas que hay
 	private Pelicula pelicula;
 	
 	public PeliculasController(WebTarget webTarget) {
 		this.webTarget = webTarget;
 	}
 	
-	
+	//Metodo que recibe las peliculas que hay y las agrupa en un ArrayList
 	public List<Pelicula> getPeliculas() {
 		WebTarget webTarget = this.webTarget.path("deustocine/peliculas");
 		List<Pelicula> lPeliculas = webTarget.request(MediaType.APPLICATION_JSON).get( new GenericType<List<Pelicula>>() {
@@ -35,7 +40,7 @@ public class PeliculasController {
 		return alPeliculas;
 	}
 
-
+	//Metodo que crea un panel por cada pelicula que haya con sus diferentes caracteristicas
 	public void crearPanelPelicula(Pelicula p, JPanel pCentro) {
 		this.pelicula = p;
 		JPanel pContenedor = new JPanel();
